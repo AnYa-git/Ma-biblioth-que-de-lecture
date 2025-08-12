1657b3db8877f68f22935f4a9399cb25f6880a84
Anya <aynabenadda16@gmail.com>
2025-08-12 20:59:55 +0100

    ajout de l'event listener sur le boutton du formulaire pout ajouter les valeur des input a la liste

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+let form = document.getElementById("ajout-livre");
+let submitButton = document.getElementById("ajouter");
+let inputTitre = document.getElementById("titre");
+let inputAuteur = document.getElementById("auteur");
+let listeLivres = document.getElementById("livres");
 let scrollTop = document.getElementById("scrollTop");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let titre = inputTitre.value.trim();
+  let auteur = inputAuteur.value.trim();
+
+  if (titre !== " " && auteur !== " ") {
+    let nouveauLivre = document.createElement("li");
+    nouveauLivre.textContent = `${titre} - ${auteur}`;
+
+    listeLivres.appendChild(nouveauLivre);
+
+    inputAuteur.value = "";
+    inputTitre.value = "";
+  }
+});
 
 scrollTop.addEventListener("click", function () {
   window.scrollTo({
