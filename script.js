7c3d50fd9595546fa8f4ff143eacaba5a636f32d
Anya <aynabenadda16@gmail.com>
2025-08-12 22:14:56 +0100

    ajout de l'evenement scroll pour afficher le scrollTop quand on descends unpeu dans la page

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let form = document.getElementById("ajout-livre");
-let submitButton = document.getElementById("ajouter");
 let inputTitre = document.getElementById("titre");
 let inputAuteur = document.getElementById("auteur");
 let listeLivres = document.getElementById("livres");
@@ -11,20 +10,41 @@ form.addEventListener("submit", function (e) {
   let titre = inputTitre.value.trim();
   let auteur = inputAuteur.value.trim();
 
-  if (titre !== " " && auteur !== " ") {
+  if (titre !== "" && auteur !== "") {
     let nouveauLivre = document.createElement("li");
     nouveauLivre.textContent = `${titre} - ${auteur}`;
 
     listeLivres.appendChild(nouveauLivre);
+
+    sauvegarderTitre(titre);
+    sauvegarderAuteur(auteur);
 
     inputAuteur.value = "";
     inputTitre.value = "";
   }
 });
 
+function sauvegarderTitre(titre, auteur) {
+  if (localStorage.length > 0) {
+    inputTitre.value = localStorage.getItem("titre");
+    inputAuteur.value = localStorage.getItem("auteur");
+  }
+
+  localStorage.setItem("titre", inputTitre.value);
+  localStorage.setItem("auteur", inputAuteur.value);
+}
+
 scrollTop.addEventListener("click", function () {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
+});
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    scrollTop.style.display = "block";
+  } else {
+    scrollTop.style.display = "none";
+  }
 });
